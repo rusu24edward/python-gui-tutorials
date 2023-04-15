2d4ca5b31dc77473c077aef1ad413b4a8ab39d8b
Ephraim Rusu <rusu24edward@gmail.com>
2023-04-15 19:30:29 -0400

    Initial setup file

---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='python-gui-tutorials',
+    version='0.0.1',
+    description='Python GUI tutorials',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rusu24edward/python-gui-tutorials',
+    author='Ephraim Rusu',
+    author_email='serephraim24@gmail.gov',
+    license='BSD 3',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Education',
+    ],
+    packages=setuptools.find_packages(),
+    # install_requires=[
+    #     'importlib-metadata<5.0',
+    #     'tensorflow',
+    #     'gym<0.22',
+    #     'ray[rllib]==2.0.0',
+    #     'open-spiel',
+    #     'matplotlib',
+    #     'seaborn',
+    # ],
+    python_requires='>=3.7, <3.11',
+    # entry_points={
+    #     'console_scripts': [
+    #         'abmarl=abmarl.scripts.scripts:cli'
+    #     ]
+    # },
+)
